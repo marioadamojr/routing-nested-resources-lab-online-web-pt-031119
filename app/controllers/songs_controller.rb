ca8941a8cbca21485d5fe709d6703ff38054efc8
Mario Adamo Jr <marioadamojr@gmail.com>
2019-09-07 18:22:42 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,7 +4,7 @@ class SongsController < ApplicationController
       if Artist.find_by(id: params[:artist_id])
         @songs = Artist.find(params[:artist_id]).songs
       else
-        rediect_to artists_path, alert: "Artist not found"
+        redirect_to artists_path, alert: "Artist not found"
       end
     else
       @songs = Song.all
